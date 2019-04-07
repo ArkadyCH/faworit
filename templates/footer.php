3e56aa6c6350df9cfdaeb4ab207bfdd640da654d
ArkadyCH <arkadychu4vaga@gmail.com>
2019-04-07 20:35:24 +0300

    fix link in footer

--- templates/footer.php
+++ templates/footer.php
@@ -3,7 +3,7 @@
         <div class="footer-links">
             <ul>
                 <li><a href="/">Главная</a></li>
-                <li><a href="catalog.php">Категории товара</a></li>
+                <li><a href="category.php">Категории товаров</a></li>
                 <li><a href="news.php">Новости</a></li>
                 <li><a href="post.php">Поставщикам</a></li>
                 <li><a href="about.php">О компании</a></li>
